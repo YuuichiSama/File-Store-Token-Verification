b70b2f66c5c3eb04351dabb0b6ea11841e42d571
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-05-12 16:34:19 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await query.message.edit_text(
             text =f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>𓆰𝒀𝒖𝒖𝒊𝒄𝒉𝒊~𝑺𝒂𝒎𝒂𓂀</a>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/Coco_ShareBot?start=Z2V0LTEwNzgyNjgyNjU4MzI4NDg'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Animes_vq'>ᴀɴɪᴍᴇ ᴜɴɪᴛʏ</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/THE_VANQUISHERS'>ᴛʜᴇ ᴠᴀɴϙᴜɪsʜᴇʀs</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/weebsunity'>ᴡᴇᴇʙs ᴜɴɪᴛʏ</a></b>",
             disable_web_page_preview = True,
-            disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
