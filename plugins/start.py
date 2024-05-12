f48f5d32b8e180fb03d75139b7ff63249fa1306c
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-05-12 16:05:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -35,7 +35,7 @@ from shortzy import Shortzy
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "10"))
+SECONDS = int(os.getenv("SECONDS", "1800"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -128,7 +128,7 @@ async def start_command(client: Client, message: Message):
                 except:
                     pass
 
-            SD = await message.reply_text("Baka! Files will be deleted After 600 seconds. Save them to the Saved Message now!")
+            SD = await message.reply_text("Baka! Files will be deleted After 30 minutes. Save them to the Saved Message now!")
             await asyncio.sleep(SECONDS)
 
             for snt_msg in snt_msgs:
